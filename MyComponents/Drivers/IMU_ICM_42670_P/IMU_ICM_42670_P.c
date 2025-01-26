a946bab715fa2faad1e1ba20defd5d998929ae24
Sadziooo <81397463+Sadziooo@users.noreply.github.com>
2025-01-26 11:57:19 +0100

    Added folders to project structure - new files for IMU driver

---
+++ MyComponents/Drivers/IMU_ICM_42670_P/IMU_ICM_42670_P.c
@@ -0,0 +1,10 @@
+/*
+ * IMU_ICM_42670_P.c
+ *
+ *  Created on: Jan 26, 2025
+ *      Author: Sadziooo
+ */
+
+#include "IMU_ICM_42670_P.h"
+
+
---
+++ MyComponents/Drivers/IMU_ICM_42670_P/IMU_ICM_42670_P.h
@@ -0,0 +1,13 @@
+/*
+ * IMU_ICM_42670_P.h
+ *
+ *  Created on: Jan 26, 2025
+ *      Author: Sadziooo
+ */
+
+#ifndef DRIVERS_IMU_ICM_42670_P_IMU_ICM_42670_P_H_
+#define DRIVERS_IMU_ICM_42670_P_IMU_ICM_42670_P_H_
+
+
+
+#endif /* DRIVERS_IMU_ICM_42670_P_IMU_ICM_42670_P_H_ */
